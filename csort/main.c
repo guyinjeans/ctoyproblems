5a20dff905ce9f5dd2765a630c94351064b45432
JD Broberg <jdbroberg@yahoo.com>
2017-10-21 13:00:08 -0700

    added forgotten return for no filename provided

--- csort/main.c
+++ csort/main.c
@@ -7,13 +7,14 @@
 int main(int argc, char* argv[]) {
 
   if (argc != 2) {
-    printf("use as %s \"filename\"", argv[0]);
+    printf("use as '%s filename'\n", argv[0]);
+    return -1;
   }
 
   FILE* fstream = fopen(argv[1], "r");
 
   if (!fstream) {
-    printf("Error opening file %s", argv[1]);
+    printf("Error opening file '%s'\n", argv[1]);
     return -1;
   }
 
